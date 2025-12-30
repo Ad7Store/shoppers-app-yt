d79bd62daddfd230a8e8164131581429461b16dc
Ad7Store <ad7store.log@gmail.com>
2025-12-30 04:14:42 -0800

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,12 +2,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyAAoNpNMx9owEpW8Hyf-RrCV717-PhN2Iw",
+  authDomain: "zaid-cef4b.firebaseapp.com",
+  projectId: "zaid-cef4b",
+  storageBucket: "zaid-cef4b.firebasestorage.app",
+  messagingSenderId: "164572288194",
+  appId: "1:164572288194:web:301261b6d36952a383575b",
+  
 };
 
 // Initialize Firebase
