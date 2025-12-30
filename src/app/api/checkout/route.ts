e62b34fec0658a093d562f2a5a809abbd7e7dc28
Ad7Store <ad7store.log@gmail.com>
2025-12-30 06:10:51 -0800

    Update route.ts

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,57 +1,41 @@
 import { ProductData } from "@/types";
 import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
 
 export const POST = async (request: NextRequest) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
   try {
     const reqBody = await request.json();
-    const { items, email, address, phone } = reqBody;
+    const { items, email } = await reqBody;
 
-    if (!items || items.length === 0) {
-      return NextResponse.json(
-        { message: "No items in order" },
-        { status: 400 }
-      );
-    }
-
-    // Calculate total
-    const orderItems = items.map((item: ProductData) => ({
-      title: item.title,
-      price: item.price,
-      quantity: item.quantity,
-      subtotal: item.price * item.quantity,
-    }));
-
-    const totalAmount = orderItems.reduce(
-      (sum, item) => sum + item.subtotal,
-      0
-    );
-
-    // Example order object (save this in DB later)
-    const order = {
-      id: crypto.randomUUID(),
-      items: orderItems,
-      totalAmount,
-      paymentMethod: "CASH_ON_DELIVERY",
-      customer: {
-        email,
-        phone,
-        address,
+    const extractingItems = await items.map((item: ProductData) => ({
+      quantity: item?.quantity,
+      price_data: {
+        currency: "usd",
+        unit_amount: Math.round(item.price * 100),
+        product_data: {
+          name: item?.title,
+          description: item?.description,
+          //   images: item?.image,
+        },
       },
-      status: "PENDING",
-      createdAt: new Date(),
-    };
+    }));
+    const origin = request.headers.get("origin");
 
-    // TODO: Save order to database (MongoDB, Prisma, Firebase, etc.)
-
-    return NextResponse.json({
-      success: true,
-      message: "Order placed successfully",
-      order,
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: extractingItems,
+      mode: "payment",
+      success_url: `${origin}/success/?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${origin}/cancel/?canceled=true`,
+      metadata: {
+        email,
+      },
     });
+
+    return NextResponse.json({ url: session.url });
   } catch (error) {
-    return NextResponse.json(
-      { message: "Failed to place order", error },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 };
